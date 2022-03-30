b33ba985bf4f20a260c5e65406244e5e724d9d43
atc-3 <cintronasani@gmail.com>
2022-03-29 18:45:42 -0700

    advice app

---
+++ advice-app/app.js
@@ -0,0 +1,19 @@
+const adviceNumber = document.querySelector('.advice__number');
+const quote = document.querySelector('.advice__quote');
+const randomQuoteButton = document.querySelector('.advice__change-quote');
+
+randomQuoteButton.addEventListener('click', getQuote);
+
+async function getQuote() {
+    let response = await fetch('https://api.adviceslip.com/advice');
+
+    if (response.status === 200) {
+        let data = await response.json();
+        setQuote(data.slip);
+    }
+}
+
+function setQuote({ advice, id }) {
+    adviceNumber.innerText = id;
+    quote.innerText = advice;
+}
